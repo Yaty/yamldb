d3c5987bacf8b87c311c33be487a41a22ec9f965
Da Roit Hugo <contact@hdaroit.fr>
2017-11-19 21:19:11 +0100

    Remove useless free + new test

--- tests/src/code/api/output.c
+++ tests/src/code/api/output.c
@@ -14,27 +14,41 @@ char *mixSequencePath = "./resources/tests/parser/mix.yml";
 static char *mapsAreCorrectlyParsedAndPrinted() {
     Node *root = YAMLParseFile(testMapsPath);
     YAMLSaveNode(root, outputPath);
-    YAMLFreeNode(root);
     mu_assert("mapsAreCorrectlyParsedAndPrinted", filesEquals(testMapsPath, outputPath));
 }
 
 static char *sequenceAreCorrectlyParsedAndPrinted() {
     Node *root = YAMLParseFile(testSequencesPath);
     YAMLSaveNode(root, outputPath);
-    YAMLFreeNode(root);
     mu_assert("sequenceAreCorrectlyParsedAndPrinted", filesEquals(testSequencesPath, outputPath));
 }
 
 static char *mixAreCorrectlyParsedAndPrinted() {
     Node *root = YAMLParseFile(mixSequencePath);
     YAMLSaveNode(root, outputPath);
-    YAMLFreeNode(root);
     mu_assert("mixAreCorrectlyParsedAndPrinted", filesEquals(mixSequencePath, outputPath));
+}
+
+static char *multipleParse() {
+    Node *root = YAMLParseFile(mixSequencePath);
+    int i;
+    int iterations = 10;
+
+    for (i = 0; i < iterations; i++) {
+        YAMLSaveNode(root, outputPath);
+        if (!filesEquals(mixSequencePath, outputPath)) {
+            break;
+        }
+        root = YAMLParseFile(outputPath);
+    }
+
+    mu_assert("multipleParse", i == iterations);
 }
 
 char *outputAllTests() {
     mu_run_test(mapsAreCorrectlyParsedAndPrinted);
     mu_run_test(sequenceAreCorrectlyParsedAndPrinted);
     mu_run_test(mixAreCorrectlyParsedAndPrinted);
+    mu_run_test(multipleParse);
     return 0;
 }
